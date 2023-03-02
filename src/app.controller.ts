778dae1fe8490556f903c82997f5d9664c970279
alvinlal <alvinzzz2001@gmail.com>
2023-03-02 17:02:38 +0530

    add test-app-2 endpoint

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
+import axios from 'axios';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
+  @Get('/')
+  async getHello(): Promise<string> {
     return this.appService.getHello();
+  }
+
+  @Get('/test-app-2')
+  async getFromTestApp2(): Promise<string> {
+    const res = await axios.get('http://test-app-2.testapp.com/');
+    console.log(res.data);
+    return res.data;
   }
 }
