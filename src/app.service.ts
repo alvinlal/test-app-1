7d5aa6a6832e61df8a2c8bd8284ba4de8da25792
alvinlal <alvinzzz2001@gmail.com>
2023-03-31 10:31:21 +0530

    buddy test again

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'hello from test-app-1 !, how are you';
+    return 'hello from test-app-1';
   }
 }
