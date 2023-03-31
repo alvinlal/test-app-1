631015e273ede68cd010f8d055e3c871ff0e1894
alvinlal <alvinzzz2001@gmail.com>
2023-03-31 11:58:20 +0530

    fix db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,15 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'test-app-rds.testapp.com',
-      port: 3306,
-      username: 'admin',
-      password: 'D&*oBE63%kmthAr%w0Vp',
-      database: 'testdb',
-      entities: [],
-    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: 'test-app-rds.testapp.com',
+    //   port: 3306,
+    //   username: 'admin',
+    //   password: 'D&*oBE63%kmthAr%w0Vp',
+    //   database: 'testdb',
+    //   entities: [],
+    // }),
   ],
   controllers: [AppController],
   providers: [AppService],
